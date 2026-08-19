4acf735687a8d4842c9e81183789b06437f51030
Nicole Cayo <nickybelu@gmail.com>
2026-08-19 15:55:56 -0300

    first commit

---
+++ src/test/java/com/nkydev/EventTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
